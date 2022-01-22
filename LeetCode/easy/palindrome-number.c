0939df00a253ff2652d8053efaf7362076d9cca0
Jack Cherng <jfcherng@gmail.com>
2022-01-22 22:22:17 +0800

    feat(lc): palindrome-number
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

---
+++ LeetCode/easy/palindrome-number.c
@@ -0,0 +1,38 @@
+#include "uthash.h"
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+bool isPalindrome(int x) {
+  if (x < 0)
+    return false;
+
+  int digits[32];
+  int digit_cnt = 0;
+  while (x) {
+    digits[digit_cnt++] = x % 10;
+    x /= 10;
+  }
+
+  for (int i = 0; i < digit_cnt / 2; ++i) {
+    if (digits[i] != digits[digit_cnt - 1 - i])
+      return false;
+  }
+
+  return true;
+}
+
+int main(int argc, char *argv[]) {
+  int input;
+
+  input = 121;
+  printf("isPalindrome(%d) = %d\n", input, isPalindrome(input));
+  input = -121;
+  printf("isPalindrome(%d) = %d\n", input, isPalindrome(input));
+  input = 10;
+  printf("isPalindrome(%d) = %d\n", input, isPalindrome(input));
+
+  return 0;
+}
