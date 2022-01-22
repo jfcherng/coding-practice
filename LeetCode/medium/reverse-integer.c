2ccdd46951b7ea35805250572eafd6c77e7f8259
Jack Cherng <jfcherng@gmail.com>
2022-01-23 01:53:46 +0800

    feat(lc): reverse-integer
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

---
+++ LeetCode/medium/reverse-integer.c
@@ -0,0 +1,54 @@
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NEGATIVE_MIN 0x80000000
+#define POSITIVE_MAX 0x7fffffff
+
+int reverse(int x) {
+  if (x == NEGATIVE_MIN)
+    return 0;
+
+  // printf("=== Process %d ===\n", x);
+
+  int sign = (x >> 31) & 1;
+
+  int digits[32] = {0};
+  int digit_cnt = 0;
+
+  // printf("sign = %d\n", sign);
+
+  x *= sign ? -1 : 1;
+  while (x) {
+    digits[digit_cnt++] = x % 10;
+    x /= 10;
+  }
+
+  int reversed = 0;
+  for (int i = 0; i < digit_cnt; ++i) {
+    // overflow
+    if (reversed >= (POSITIVE_MAX - digits[i]) / 10.0) {
+      return 0;
+    }
+
+    reversed = reversed * 10 + digits[i];
+  }
+
+  // printf("reversed = %d\n", reversed);
+
+  return reversed * (sign ? -1 : 1);
+}
+
+int main(int argc, char *argv[]) {
+  int input;
+
+  input = 123;
+  printf("reverse(%d) = %d\n", input, reverse(input));
+  input = -123;
+  printf("reverse(%d) = %d\n", input, reverse(input));
+  input = 120;
+  printf("reverse(%d) = %d\n", input, reverse(input));
+
+  return 0;
+}
