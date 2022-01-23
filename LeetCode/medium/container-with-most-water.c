362c3a390c95312bb87e9ca07d29bc1c25d65f3d
Jack Cherng <jfcherng@gmail.com>
2022-01-23 22:24:22 +0800

    feat(lc): container-with-most-water
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

---
+++ LeetCode/medium/container-with-most-water.c
@@ -0,0 +1,33 @@
+#include "uthash.h"
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MIN(x, y) ((x) < (y) ? (x) : (y))
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+
+int maxArea(int *height, int heightSize) {
+  int max = 0;
+  int l = 0, r = heightSize - 1;
+
+  while (l < r) {
+    max = MAX(max, (r - l) * MIN(height[l], height[r]));
+    if (height[l] < height[r]) {
+      ++l;
+    } else {
+      --r;
+    }
+  }
+
+  return max;
+}
+
+int main(int argc, char *argv[]) {
+  int heights[] = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+
+  printf("ans_1 = %d (49)\n", maxArea(heights, sizeof(heights) / sizeof(int)));
+
+  return 0;
+}
