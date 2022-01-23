47ea3878655aabd16644bd6485d1cde0cdd240e6
Jack Cherng <jfcherng@gmail.com>
2022-01-24 01:45:52 +0800

    feat(lc): search-insert-position
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

---
+++ LeetCode/easy/search-insert-position.c
@@ -0,0 +1,32 @@
+#include "uthash.h"
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int searchInsert(int *nums, int numsSize, int target) {
+  if (numsSize == 0)
+    return 0;
+
+  int l = 0, r = numsSize - 1;
+
+  while (l < r) {
+    int mid = (l + r) / 2;
+
+    if (nums[mid] == target) {
+      return mid;
+    }
+
+    if (nums[mid] > target) {
+      r = mid - 1;
+    } else {
+      l = mid + 1;
+    }
+  }
+
+  return nums[l] < target ? l + 1 : l;
+}
+
+int main(int argc, char *argv[]) {
+  return 0;
+}
