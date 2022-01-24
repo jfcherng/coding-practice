3f2778bc127fb1c6776afdba14485b632b1cb4cf
Jack Cherng <jfcherng@gmail.com>
2022-01-25 01:53:47 +0800

    feat(lc): maximum-number-of-balloons
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

---
+++ LeetCode/easy/1189-maximum-number-of-balloons.c
@@ -0,0 +1,43 @@
+#include "uthash.h"
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MIN(X, Y) ((X) < (Y) ? (X) : (Y))
+
+int maxNumberOfBalloons(char *text) {
+  // "balloon" = a1, b1, l2, n1, o1
+  int res = 0;
+
+  int a_cnt, b_cnt, l_cnt, n_cnt, o_cnt;
+  a_cnt = b_cnt = l_cnt = n_cnt = o_cnt = 0;
+
+  while (*text) {
+    switch (*text) {
+    case 'a':
+      a_cnt++;
+      break;
+    case 'b':
+      b_cnt++;
+      break;
+    case 'l':
+      l_cnt++;
+      break;
+    case 'n':
+      n_cnt++;
+      break;
+    case 'o':
+      o_cnt++;
+      break;
+    }
+    ++text;
+  }
+
+  return MIN(MIN(MIN(MIN(a_cnt, b_cnt), l_cnt / 2), n_cnt), o_cnt / 2);
+}
+
+int main(int argc, char *argv[]) {
+  return 0;
+}
