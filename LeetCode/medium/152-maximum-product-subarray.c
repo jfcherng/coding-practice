0611ded44b02b757ce0e5bf6018d8c0c9fd99b51
Jack Cherng <jfcherng@gmail.com>
2022-01-25 16:58:51 +0800

    feat(lc): 152-maximum-product-subarray
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

---
+++ LeetCode/medium/152-maximum-product-subarray.c
@@ -0,0 +1,24 @@
+#include "_leetcode_common.h"
+
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+#define MIN(x, y) ((x) < (y) ? (x) : (y))
+
+int maxProduct(int *nums, int numsSize) {
+  int res = nums[0];
+  int min = 1;
+  int max = 1;
+
+  for (int i = 0; i < numsSize; ++i) {
+    int from_min = nums[i] * min;
+    int from_max = nums[i] * max;
+    min = MIN(MIN(from_min, from_max), nums[i]);
+    max = MAX(MAX(from_min, from_max), nums[i]);
+    res = MAX(res, max);
+  }
+
+  return res;
+}
+
+int main(int argc, char *argv[]) {
+  return 0;
+}
