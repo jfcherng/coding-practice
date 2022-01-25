f6156f5583789e89c9b8cc762e4ec082559a4a2f
Jack Cherng <jfcherng@gmail.com>
2022-01-25 17:07:14 +0800

    feat(lc): 136-single-number
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

---
+++ LeetCode/easy/136-single-number.c
@@ -0,0 +1,12 @@
+#include "_leetcode_common.h"
+
+int singleNumber(int *nums, int numsSize) {
+  int res = 0;
+  for (int i = 0; i < numsSize; ++i)
+    res ^= nums[i];
+  return res;
+}
+
+int main(int argc, char *argv[]) {
+  return 0;
+}
