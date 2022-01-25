9c6ccee840e5c5b5e12a29b3c804e82a31d3abaf
Jack Cherng <jfcherng@gmail.com>
2022-01-25 14:56:53 +0800

    feat(lc): 153-find-minimum-in-rotated-sorted-array
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

---
+++ LeetCode/medium/153-find-minimum-in-rotated-sorted-array.c
@@ -0,0 +1,34 @@
+#include "_leetcode_common.h"
+
+#define MIN(x, y) ((x) < (y) ? (x) : (y))
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+
+int findMin(int *nums, int numsSize) {
+  int res = nums[0];
+
+  int l = 0, r = numsSize - 1;
+
+  while (l <= r) {
+    int mid = (l + r) / 2;
+
+    res = MIN(res, nums[mid]);
+
+    // l~mid is sorted
+    if (nums[l] < nums[mid]) {
+      res = MIN(res, nums[l]);
+      l = mid + 1;
+    }
+    // mid~r is sorted
+    else {
+      if (mid < numsSize - 1)
+        res = MIN(res, nums[mid + 1]);
+      r = mid - 1;
+    }
+  }
+
+  return res;
+}
+
+int main(int argc, char *argv[]) {
+  return 0;
+}
