223c746d18cad053f7ba48bdef1b6289f790dbc6
Jack Cherng <jfcherng@gmail.com>
2022-01-28 22:55:15 +0800

    feat(lc): 55-jump-game
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

---
+++ LeetCode/medium/55-jump-game.c
@@ -0,0 +1,31 @@
+#include "_leetcode_common.h"
+
+bool canJump(int *nums, int numsSize) {
+  if (numsSize <= 0) {
+    return false;
+  }
+
+  // let dp[i] = i-th index can be reached or not
+  bool *dp = malloc(numsSize * sizeof(*dp));
+  dp[0] = true;
+
+  for (int i = 1; i < numsSize; ++i) {
+    dp[i] = false;
+    // i index comes from i-1 index + 1 jump,
+    //                    i-2 index + 2 jumps,
+    //                    ...,
+    //                    or 0 index + i jumps
+    for (int j = i - 1; j >= 0; --j) {
+      dp[i] |= dp[j] && nums[j] >= i - j;
+      if (dp[i]) {
+        break;
+      }
+    }
+  }
+
+  return dp[numsSize - 1];
+}
+
+int main(int argc, char *argv[]) {
+  return 0;
+}
