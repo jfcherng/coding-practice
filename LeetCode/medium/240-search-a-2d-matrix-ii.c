cb76c14cfac2dedf1310da33f345de73a5e2b5f1
Jack Cherng <jfcherng@gmail.com>
2022-01-29 02:27:30 +0800

    feat(lc): 240-search-a-2d-matrix-ii
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

---
+++ LeetCode/medium/240-search-a-2d-matrix-ii.c
@@ -0,0 +1,27 @@
+#include "_leetcode_common.h"
+
+bool searchMatrix(int **matrix, int matrixSize, int *matrixColSize, int target) {
+  int m = matrixSize, n = m ? matrixColSize[0] : 0;
+
+  // from the top-right corner,
+  // if target > value, then row++
+  //           <             column--
+  //           =             BINGO!
+
+  int r = 0, c = n - 1;
+  while (r < m && c >= 0) {
+    if (matrix[r][c] > target) {
+      --c;
+    } else if (matrix[r][c] < target) {
+      ++r;
+    } else {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+int main(int argc, char *argv[]) {
+  return 0;
+}
