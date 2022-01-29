0f23a41b619dfb6452ec372e1656c58deba34197
Jack Cherng <jfcherng@gmail.com>
2022-01-29 23:55:07 +0800

    feat(lc): 547-number-of-provinces
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

---
+++ LeetCode/medium/547-number-of-provinces.c
@@ -0,0 +1,36 @@
+#include "_leetcode_common.h"
+
+void explore(int **isConnected, bool *visited, int n, int node) {
+  if (visited[node])
+    return;
+
+  visited[node] = true;
+  for (int i = 0; i < n; ++i) {
+    if (isConnected[i][node]) {
+      explore(isConnected, visited, n, i);
+    }
+  }
+}
+
+int findCircleNum(int **isConnected, int isConnectedSize, int *isConnectedColSize) {
+  int n = isConnectedSize; // n x n matrix
+  int res = 0;
+
+  bool *visited = malloc(n * sizeof(*visited));
+  for (int i = 0; i < n; ++i) {
+    visited[i] = false;
+  }
+
+  for (int i = 0; i < n; ++i) {
+    if (!visited[i]) {
+      ++res;
+      explore(isConnected, visited, n, i);
+    }
+  }
+
+  return res;
+}
+
+int main(int argc, char *argv[]) {
+  return 0;
+}
