7e8f438ea8c2e89eb97d18ab80e133a21c6a9232
Jack Cherng <jfcherng@gmail.com>
2022-01-29 14:09:20 +0800

    refactor(lc): improve 438-find-all-anagrams-in-a-string
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- LeetCode/medium/438-find-all-anagrams-in-a-string.c
+++ LeetCode/medium/438-find-all-anagrams-in-a-string.c
@@ -1,29 +1,4 @@
 #include "_leetcode_common.h"
-#include <stdio.h>
-
-typedef struct {
-  int chr;
-  int cnt;
-  UT_hash_handle hh;
-} Counter;
-
-Counter *createNewCounter(int chr) {
-  Counter *new = malloc(sizeof(*new));
-  new->chr = chr;
-  new->cnt = 1;
-  return new;
-}
-
-void pop_at_l(Counter *pool, char *s, int *l, int *total_cnt) {
-  Counter *out;
-  int key = s[*l];
-  HASH_FIND_INT(pool, &key, out);
-  if (out) {
-    ++out->cnt;
-    ++*total_cnt;
-  }
-  ++*l;
-}
 
 /**
  * Note: The returned array must be malloced, assume caller calls free().
@@ -32,46 +7,38 @@ int *findAnagrams(char *s, char *p, int *returnSize) {
   int *res = malloc(strlen(s) * sizeof(*res));
   *returnSize = 0;
 
-  int p_len = strlen(p);
-  int s_len = strlen(s);
-  Counter *out, *pool = NULL;
+  const int pLen = strlen(p);
+  const int sLen = strlen(s);
+  if (sLen < pLen)
+    return res;
 
   // count chars in p
-  int total_cnt = p_len;
-  for (int i = 0; i < total_cnt; ++i) {
-    int key = p[i];
-    HASH_FIND_INT(pool, &key, out);
-    if (out) {
-      ++out->cnt;
-      continue;
-    }
-    Counter *tmp = createNewCounter(p[i]);
-    HASH_ADD_INT(pool, chr, tmp);
+  int pCounter[26] = {0};
+  for (int i = 0; i < pLen; ++i) {
+    ++pCounter[p[i] - 'a'];
   }
 
+  int diffCount = pLen;
   int l = 0, r = 0;
-  while (r < s_len) {
-    int key = s[r];
-    HASH_FIND_INT(pool, &key, out);
-    if (out) {
-      if (out->cnt > 0) {
-        --out->cnt;
-        --total_cnt;
-        ++r;
-      } else {
-        while (s[l] != s[r]) {
-          pop_at_l(pool, s, &l, &total_cnt);
-        }
-        pop_at_l(pool, s, &l, &total_cnt);
-      }
+  while (r < sLen) {
+    if (pCounter[s[r] - 'a'] > 0) {
+      --pCounter[s[r] - 'a'];
+      --diffCount;
+      ++r;
     } else {
-      while (l < r) {
-        pop_at_l(pool, s, &l, &total_cnt);
+      while (l < r && pCounter[s[r] - 'a'] == 0) {
+        ++pCounter[s[l] - 'a'];
+        ++diffCount;
+        ++l;
       }
-      l = r = r + 1;
+      // the char at r is not in p, skip that char
+      if (pCounter[s[r] - 'a'] == 0) {
+        l = r = r + 1;
+        continue;
+      }
     }
 
-    if (total_cnt == 0) {
+    if (diffCount == 0) {
       res[(*returnSize)++] = l;
     }
   }
