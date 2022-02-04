ed0c968acf81d062d907c3656ac4244a9fac4b34
Jack Cherng <jfcherng@gmail.com>
2022-02-04 17:00:27 +0800

    feat(lc): 525-contiguous-array
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

---
+++ LeetCode/medium/525-contiguous-array.c
@@ -0,0 +1,50 @@
+#include "_leetcode_common.h"
+
+#define MIN(x, y) ((x) < (y) ? (x) : (y))
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+
+typedef struct {
+  int sum; // key
+  int idx;
+  UT_hash_handle hh;
+} Counter;
+
+Counter *createNewCounter(int sum, int idx) {
+  Counter *new = malloc(sizeof(*new));
+  new->sum = sum;
+  new->idx = idx;
+  return new;
+}
+
+int findMaxLength(int *nums, int numsSize) {
+  int res = 0;
+
+  Counter *out, *pool = NULL;
+  int sum = 0;
+
+  out = createNewCounter(0, -1);
+  HASH_ADD_INT(pool, sum, out);
+
+  for (int i = 0; i < numsSize; ++i) {
+    sum += nums[i] == 0 ? -1 : 1;
+
+    HASH_FIND_INT(pool, &sum, out);
+    if (out) {
+      res = MAX(res, i - out->idx);
+    } else {
+      out = createNewCounter(sum, i);
+      HASH_ADD_INT(pool, sum, out);
+    }
+  }
+
+  return res;
+}
+
+int main(int argc, char *argv[]) {
+  int nums_[] = {0, 1, 1, 0, 1, 1, 1, 0};
+  int *nums = &nums_[0];
+
+  printf("findMaxLength = %d (gold = %d)\n", findMaxLength(nums, sizeof(nums_) / sizeof(int)), 4);
+
+  return 0;
+}
