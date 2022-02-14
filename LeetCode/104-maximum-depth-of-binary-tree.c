ae0624347291641da88a1a10a3ee00794e61b48c
Jack Cherng <jfcherng@gmail.com>
2022-02-14 11:37:03 +0800

    feat(lc): 104-maximum-depth-of-binary-tree
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

---
+++ LeetCode/104-maximum-depth-of-binary-tree.c
@@ -0,0 +1,40 @@
+#include "_leetcode_common.h"
+
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+#define MIN(x, y) ((x) < (y) ? (x) : (y))
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+void inorder(struct TreeNode *root, int *curr_depth, int *max_depth) {
+  if (!root) {
+    *max_depth = MAX(*max_depth, *curr_depth);
+    return;
+  }
+
+  ++*curr_depth;
+
+  inorder(root->left, curr_depth, max_depth);
+  inorder(root->right, curr_depth, max_depth);
+
+  --*curr_depth;
+}
+
+int maxDepth(struct TreeNode *root) {
+  int res = 0;
+  int curr_depth = 0;
+
+  inorder(root, &curr_depth, &res);
+
+  return res;
+}
+
+int main(int argc, char *argv[]) {
+  return 0;
+}
