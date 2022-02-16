90a869baea13ceb61c4cc7001ea45ed228218e80
Jack Cherng <jfcherng@gmail.com>
2022-02-17 03:21:13 +0800

    feat(lc): 24-swap-nodes-in-pairs
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

---
+++ LeetCode/24-swap-nodes-in-pairs.c
@@ -0,0 +1,28 @@
+#include "_leetcode_common.h"
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+struct ListNode *swapPairs(struct ListNode *head) {
+  struct ListNode **headp = &head, *prev, *curr;
+
+  // "headp" points to the "head" of each iteration
+  while ((prev = *headp) && (curr = prev->next)) {
+    prev->next = curr->next;
+    curr->next = prev;
+    *headp = curr;
+    // prepare for the next iteration
+    headp = &(prev->next);
+  }
+
+  return head;
+}
+
+int main(int argc, char *argv[]) {
+  return 0;
+}
