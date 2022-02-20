626d4eed7041e158eb0c34cc874be56fae3acc6b
Jack Cherng <jfcherng@gmail.com>
2022-02-20 20:24:46 +0800

    feat(lc): 10-regular-expression-matching
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

---
+++ LeetCode/10-regular-expression-matching.c
@@ -0,0 +1,25 @@
+#include "_leetcode_common.h"
+
+bool isMatch(char *s, char *p) {
+  if (!*p)
+    return !*s;
+
+  bool isCurrMatched = *s && (*s == *p || *p == '.');
+
+  if (p[1] == '*') {
+    return (isCurrMatched && isMatch(s + 1, p)) || // use '*'
+           isMatch(s, p + 2)                       // not use '*'
+        ;
+  }
+
+  return isCurrMatched && isMatch(s + 1, p + 1);
+}
+
+int main(int argc, char *argv[]) {
+  // bool res = isMatch("ab", ".*c");
+  bool res = isMatch("aaaaaaaaaaaaab", "a*a*a*a*a*a*a*a*a*a*c");
+
+  printf("res = %s\n", res ? "true" : "false");
+
+  return 0;
+}
