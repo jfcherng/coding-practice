e0e1b4b321d8a09924a5c98f24c37d20e3f75926
Jack Cherng <jfcherng@gmail.com>
2022-02-20 20:58:27 +0800

    feat(lc): 20-valid-parentheses
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

---
+++ LeetCode/20-valid-parentheses.c
@@ -0,0 +1,33 @@
+#include "_leetcode_common.h"
+
+const int PAIRS_CNT = 3;
+const char *PAIRS[] = {"()", "[]", "{}"};
+
+bool isValid(char *s) {
+  int top = -1; // use "s" as in-place stack
+
+  for (int i = 0; s[i]; ++i) {
+    bool isOpening = true;
+    for (int j = 0; j < PAIRS_CNT; ++j) {
+      // is closing bracket?
+      if (s[i] == PAIRS[j][1]) {
+        isOpening = false;
+        // pair unmatched
+        if (top == -1 || s[top] != PAIRS[j][0])
+          return false;
+        // pair matched
+        --top;
+        break;
+      }
+    }
+
+    if (isOpening)
+      s[++top] = s[i];
+  }
+
+  return top == -1; // empty stack
+}
+
+int main(int argc, char *argv[]) {
+  return 0;
+}
