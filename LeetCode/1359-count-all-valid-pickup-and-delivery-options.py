c40e7da082103f368e6b88bb914a26a82fedafe7
Jack Cherng <jfcherng@gmail.com>
2022-03-07 00:54:13 +0800

    feat(lc): 1359-count-all-valid-pickup-and-delivery-options
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

---
+++ LeetCode/1359-count-all-valid-pickup-and-delivery-options.py
@@ -0,0 +1,32 @@
+class Solution:
+    def countOrders(self, n: int) -> int:
+        MOD = int(1e9 + 7)
+        dp = [[0] * (n + 1) for _ in range(n + 1)]
+
+        for unpicked in range(n + 1):
+            for undelivered in range(unpicked, n + 1):
+                # If all orders are picked and delivered then,
+                # for remaining '0' orders we have only one way.
+                if not unpicked and not undelivered:
+                    dp[unpicked][undelivered] = 1
+                    continue
+
+                # There are some unpicked elements left.
+                # We have choice to pick any one of those orders.
+                if unpicked > 0:
+                    dp[unpicked][undelivered] += unpicked * dp[unpicked - 1][undelivered]
+                    dp[unpicked][undelivered] %= MOD
+
+                # Number of deliveries done is less than picked orders.
+                # We have choice to deliver any one of (undelivered - unpicked) orders.
+                if undelivered > unpicked:
+                    dp[unpicked][undelivered] += (undelivered - unpicked) * dp[unpicked][undelivered - 1]
+                    dp[unpicked][undelivered] %= MOD
+
+        return dp[n][n]
+
+
+s = Solution()
+res = s.countOrders(1994)
+
+print(f"{res = }")
