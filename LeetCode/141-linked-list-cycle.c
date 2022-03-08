56da3706ad15963812f494816360b8a90a3900ac
Jack Cherng <jfcherng@gmail.com>
2022-03-08 12:14:26 +0800

    fix: 141-linked-list-cycle
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- LeetCode/141-linked-list-cycle.c
+++ LeetCode/141-linked-list-cycle.c
@@ -3,7 +3,7 @@
 bool hasCycle(struct ListNode *head) {
   struct ListNode *fast = head, *slow = head;
 
-  while (!fast || !fast->next) {
+  while (fast && fast->next) {
     slow = slow->next;
     fast = fast->next->next;
 
