01b3824199b937278d09739100834d06732f2ca4
Jack Cherng <jfcherng@gmail.com>
2022-03-15 16:29:51 +0800

    refactor(lc): 78-subsets
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- LeetCode/78-subsets.c
+++ LeetCode/78-subsets.c
@@ -1,35 +1,31 @@
 #include "_leetcode_common.c"
 
-/**
- * Return an array of arrays of size *returnSize.
- * The sizes of the arrays are returned as *returnColumnSizes array.
- * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
- */
+// determinate whether the `n`th bit in `x` is 1
+#define nthBit(x, n) (((x) >> (n)) & 1)
+
 int **subsets(int *nums, int numsSize, int *returnSize, int **returnColumnSizes) {
   const int total = 1 << numsSize;
-  *returnSize = total;
 
   int **res = malloc(total * sizeof(*res));
   *returnColumnSizes = malloc(total * sizeof(**returnColumnSizes));
+  *returnSize = 0;
 
-  int res_idx = 0;
   // loop from 000 to 111 (8 iterations)
   for (int i = 0; i < total; ++i) {
     int *sub = malloc(numsSize * sizeof(*res));
     int sub_idx = 0;
 
+    // check whether we take jth digit
     for (int j = 0; j < numsSize; ++j) {
       // taken
-      if ((i >> j) & 1) {
+      if (nthBit(i, j)) {
         sub[sub_idx++] = nums[j];
       }
     }
-    (*returnColumnSizes)[res_idx] = sub_idx;
 
-    // not necessary, but may save some memory
-    sub = realloc(sub, sub_idx * sizeof(*sub));
-
-    res[res_idx++] = sub;
+    (*returnColumnSizes)[*returnSize] = sub_idx;
+    sub = realloc(sub, sub_idx * sizeof(*sub)); // not necessary, but may save some memory
+    res[(*returnSize)++] = sub;
   }
 
   return res;
