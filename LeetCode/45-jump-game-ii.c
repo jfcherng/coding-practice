be217f31ea2bf2555b9e3a766d6fe5b5dd8ce4f1
Jack Cherng <jfcherng@gmail.com>
2022-03-19 11:15:39 +0800

    refactor(lc): 45-jump-game-ii
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- LeetCode/45-jump-game-ii.c
+++ LeetCode/45-jump-game-ii.c
@@ -5,28 +5,25 @@
 #define MIN(x, y) ((x) < (y) ? (x) : (y))
 #define MAX(x, y) ((x) > (y) ? (x) : (y))
 
+// time complexity = O(n), space complexity = O(1)
 int jump(int *nums, int numsSize) {
-  if (numsSize <= 1) {
-    return 0;
-  }
+  int step = 0;
 
-  int res = 0, l = 0, r = 0;
+  int l = 0, r = 0;
   while (r < numsSize - 1) {
-    int jumpTo = 0;
-    for (int j = l; j <= r; ++j) {
+    int jumpTo = r;
+    for (int j = l; j <= r; ++j)
       jumpTo = MAX(jumpTo, nums[j] + j);
-    }
 
-    if (jumpTo == r) {
+    if (jumpTo == r)
       return UNREACHABLE;
-    }
 
     l = r + 1;
     r = jumpTo;
-    ++res;
+    ++step;
   }
 
-  return res;
+  return step;
 }
 
 int main(int argc, char *argv[]) {
