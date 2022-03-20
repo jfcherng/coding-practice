9331543258488ca23db10ae8fe785d0a7daefcd5
Jack Cherng <jfcherng@gmail.com>
2022-03-20 20:52:12 +0800

    refactor(lc): 121-best-time-to-buy-and-sell-stock
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- LeetCode/121-best-time-to-buy-and-sell-stock.c
+++ LeetCode/121-best-time-to-buy-and-sell-stock.c
@@ -1,25 +1,35 @@
 #include "_leetcode_common.c"
-#include <stdlib.h>
 
 #define MIN(x, y) ((x) < (y) ? (x) : (y))
 #define MAX(x, y) ((x) > (y) ? (x) : (y))
 
 int maxProfit(int *prices, int pricesSize) {
-  if (pricesSize <= 1) {
-    return 0;
+  int max_profit = 0;
+
+  int buyAt = prices[0];
+  for (int i = 1; i < pricesSize; ++i) {
+    int profit = prices[i] - buyAt;
+
+    if (profit < 0) {
+      buyAt = prices[i];
+    } else if (profit > max_profit) {
+      max_profit = profit;
+    }
   }
 
+  return max_profit;
+}
+
+int maxProfit_dp(int *prices, int pricesSize) {
   // dp[i] = the max value after i-th (excludesive)
   int *dp = malloc(pricesSize * sizeof(*dp));
   dp[pricesSize - 1] = -INT_MAX;
-  for (int i = pricesSize - 2; i >= 0; --i) {
+  for (int i = pricesSize - 2; i >= 0; --i)
     dp[i] = MAX(dp[i + 1], prices[i + 1]);
-  }
 
   int max_profit = 0;
-  for (int i = 0; i < pricesSize - 1; ++i) {
+  for (int i = 0; i < pricesSize - 1; ++i)
     max_profit = MAX(max_profit, dp[i] - prices[i]);
-  }
   return max_profit;
 }
 
