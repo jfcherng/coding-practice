af5e5c45bdaebf01feaaa25d598a390688b31a21
Jack Cherng <jfcherng@gmail.com>
2022-03-20 13:18:09 +0800

    refactor(lc): 5-longest-palindromic-substring
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- LeetCode/5-longest-palindromic-substring.c
+++ LeetCode/5-longest-palindromic-substring.c
@@ -1,52 +1,46 @@
 #include "_leetcode_common.c"
 
 char *longestPalindrome(char *s) {
-  printf("input: %s\n", s);
   int len = strlen(s);
 
   // init DP table
   // is_palindromic[i][j] = s[i..j] is palindromic or not
-  bool *tmp = malloc(len * len * sizeof(*tmp));
   bool **is_palindromic = malloc(len * sizeof(*is_palindromic));
-  for (int i = 0; i < len; ++i) {
-    is_palindromic[i] = &tmp[i * len];
-  }
+  bool *tmp = malloc(len * len * sizeof(*tmp));
+  for (int i = 0; i < len; ++i)
+    is_palindromic[i] = tmp + i * len;
 
-  // initial conditions
+  // trivial initial conditions
   for (int i = 0; i < len; ++i) {
-    // length=1 substring
+    // length=1 substring is palindromic
     is_palindromic[i][i] = true;
-    // length=2 substring
+    // length=2 substring is palindromic if they are the same
     if (i + 1 < len)
       is_palindromic[i][i + 1] = s[i] == s[i + 1];
   }
 
   // fill table
-  for (int length = 3; length < len; ++length) {
-    for (int l = 0; l < len; ++l) {
+  for (int length = 3; length <= len; ++length) {
+    for (int l = 0; l <= len - length; ++l) {
       int r = l + (length - 1);
-      if (r >= len)
-        continue;
       is_palindromic[l][r] = is_palindromic[l + 1][r - 1] && s[l] == s[r];
-      printf("is_palindromic[%d][%d] = %d\n", l, r, is_palindromic[l][r]);
+      // printf("is_palindromic[%d][%d] = %d\n", l, r, is_palindromic[l][r]);
     }
   }
 
-  // find max
-  int res_l = 0, res_r = 0;
-  for (int l = 0; l < len; ++l) {
-    for (int r = l; r < len; ++r) {
-      if (is_palindromic[l][r] && res_r - res_l < r - l) {
+  // find max length
+  int res_l = 0, res_len = 1;
+  for (int l = 0; l <= len - res_len; ++l) {
+    for (int r = l + res_len; r < len; ++r) {
+      if (is_palindromic[l][r] && res_len < r - l + 1) {
         res_l = l;
-        res_r = r;
+        res_len = r - l + 1;
       }
     }
   }
 
-  int res_len = res_r - res_l + 1;
-  char *res = malloc((res_len + 1) * sizeof(*res));
+  char *res = calloc(res_len + 1, sizeof(*res));
   strncpy(res, s + res_l, res_len);
-  res[res_len] = '\0';
 
   free(tmp);
   free(is_palindromic);
@@ -55,6 +49,6 @@ char *longestPalindrome(char *s) {
 }
 
 int main(int argc, char *argv[]) {
-  printf("ans(%s) = %s\n", "babad", longestPalindrome("babad"));
+  printf("longestPalindrome(%s) = %s\n", "babad", longestPalindrome("babad"));
   return 0;
 }
