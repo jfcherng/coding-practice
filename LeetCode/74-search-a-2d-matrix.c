32697f19d174a1ba5c74bdb270eea0c0c5d7b584
Jack Cherng <jfcherng@gmail.com>
2022-03-30 13:46:20 +0800

    refactor(lc): 74-search-a-2d-matrix
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- LeetCode/74-search-a-2d-matrix.c
+++ LeetCode/74-search-a-2d-matrix.c
@@ -5,11 +5,9 @@ bool searchMatrix(int **matrix, int matrixSize, int *matrixColSize, int target) 
   int numsSize = rowSize * colSize;
 
   int l = 0, r = numsSize - 1;
-  while (l < r) {
+  while (l <= r) {
     int mid = l + (r - l) / 2;
-    int mid_row = mid / rowSize;
-    int mid_col = mid % rowSize;
-    int pivot = matrix[mid_row][mid_col];
+    int pivot = matrix[mid / rowSize][mid % rowSize];
 
     if (pivot < target) {
       l = mid + 1;
