131d51367bf20f783bbdde3e532a426e25d2b6e5
Jack Cherng <jfcherng@gmail.com>
2022-03-31 20:19:35 +0800

    feat(lc): 410-split-array-largest-sum
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

---
+++ LeetCode/410-split-array-largest-sum.py
@@ -0,0 +1,47 @@
+from typing import List
+import itertools
+
+
+class Solution:
+    def splitArray_dp(self, nums: List[int], m: int) -> int:
+        n = len(nums)
+
+        # prefix sum, ps[i] = nums[0] + ... + num[i-1]
+        ps = [0] + list(itertools.accumulate(nums))
+
+        # dp[i][j] = min largest sum for nums[i..n) where m = j
+        dp = [[-1] * (m + 1) for _ in range(n)]
+        # base cases (m = 1)
+        for i in range(n):
+            dp[i][1] = ps[n] - ps[i]
+
+        # fill table
+        for j in range(2, m + 1):
+            for i in range(n):
+                min_res = ps[n]  # upper bound
+                # for m = j, we split nums[i..n) into nums[i..k] and nums[k+1..n)
+                # where nums[i..k] is the `j`th split and min largest sum of nums[k+1..n) is dp[k+1][j-1]
+                for k in range(i, n - (j - 1)):
+                    min_res = min(
+                        min_res,
+                        max(
+                            split_sum := ps[k + 1] - ps[i],  # sum of the `j`th split
+                            dp[k + 1][j - 1],  # the largest sum of other `j-1` splits
+                        ),
+                    )
+                    if split_sum >= min_res:
+                        break
+                dp[i][j] = min_res
+
+        return dp[0][m]
+
+    splitArray = splitArray_dp
+
+
+s = Solution()
+
+res = s.splitArray([7, 2, 5, 10, 8], 2)
+print(f"{res = } (18)")
+
+res = s.splitArray([1, 4, 4], 3)
+print(f"{res = } (4)")
