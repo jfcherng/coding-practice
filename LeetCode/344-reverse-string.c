e8a4c0aa2b757f4a584fddaeabe590fbefc2d9ef
Jack Cherng <jfcherng@gmail.com>
2022-04-01 10:31:05 +0800

    refactor(lc): 344-reverse-string
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- LeetCode/344-reverse-string.c
+++ LeetCode/344-reverse-string.c
@@ -1,11 +1,11 @@
 #include "_leetcode_common.c"
 
 void reverseString(char *s, int sSize) {
-  char tmp;
-  for (int i = 0; i < sSize / 2; ++i) {
-    tmp = s[i];
-    s[i] = s[sSize - 1 - i];
-    s[sSize - 1 - i] = tmp;
+  for (int i = (sSize >> 1) - 1; i >= 0; --i) {
+    // swap index `i` and index `sSize-1-i`
+    s[i] ^= s[sSize - 1 - i];
+    s[sSize - 1 - i] ^= s[i];
+    s[i] ^= s[sSize - 1 - i];
   }
 }
 
